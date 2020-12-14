d70f27c4ba48c0b1dafa632dc0622a43dc087724
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2020-12-14 19:13:54 +0700

    the second step

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ brain-games:
 
 publish:
 	npm publish --dry-run
+
+init:
+	npx eslint .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import requestName from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+requestName();
